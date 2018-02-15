769de53d9570805a5665b8b1ba0fb56a371d6061
Stylix <ugopezzulla@gmail.com>
2018-02-15 11:56:57 +0100

    Update otrChoz.js

--- otrChoz.js
+++ otrChoz.js
@@ -1,2 +1,31 @@
-//To initialize OtrChoz.js
-console.log('Your life is better now !');
+/* 
+ * OtrChoz.js 0.1.0
+ * (c) Stylix
+ Release under Yolo nothing licence.
+*/
+
+/*
+   ____  _         _____ _                _     
+  / __ \| |       / ____| |              (_)    
+ | |  | | |_ _ __| |    | |__   ___ ____  _ ___ 
+ | |  | | __| '__| |    | '_ \ / _ \_  / | / __|
+ | |__| | |_| |  | |____| | | | (_) / / _| \__ \
+  \____/ \__|_|   \_____|_| |_|\___/___(_) |___/
+                                        _/ |    
+                                       
+*/
+
+
+/**
+ * @constructor
+ * @param {Object} options - The options object.
+ */
+function OtrChoz( options ) {
+  this.start();
+}
+
+function start(){
+  console.log('Your life is better now !');
+}
+
+
